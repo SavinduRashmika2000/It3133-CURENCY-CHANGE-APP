d67d8cdd9bce5e1731cf5d4b545ca737333a619b
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-12-25 16:44:17 +0530

    make home components and Finish

--- CurencyApp/components/Home.js
+++ CurencyApp/components/Home.js
@@ -1,6 +1,6 @@
-import { View, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
-import { PaperProvider, Text, Divider } from 'react-native-paper';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
 
 
 
@@ -8,12 +8,58 @@ import { PaperProvider, Text, Divider } from 'react-native-paper';
 export default function Home() {
     
     const [amountLKR, setAmountLKR] = useState('');
-    const [convertedUSD, setConvertedUSD] = useState(null);
+    const [amountUSD, setAmountUSD] = useState(null);
     const exchangeRate = 0.0030;
    
+    const convertCurrency = () => {
+        const lkr = parseFloat(amountLKR);
+        if (!isNaN(lkr)) {
+          setAmountUSD((lkr * exchangeRate).toFixed(2));
+        } else {
+          setAmountUSD(null);
+          alert('Please enter a valid amount.');
+        }
+      };
+    
     return (
-        <view>
+        <View style={styles.container}>
+            <TextInput
+                label="LKR Currency"
+                mode="outlined"
+                value={amountLKR}
+                onChangeText={setAmountLKR}
+                style={styles.input} />
+            <TextInput
+                label="USD Currency"
+                mode="outlined"
+                value={amountUSD}
+                style={styles.input} />
 
-        </view>
+            <Button mode="contained" onPress={convertCurrency}>
+                Convert
+            </Button>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 20,
+      backgroundColor: '#F0F8FF',
+    },
+    input: {
+      height: 50,
+      width: '100%',
+      marginVertical: 10,
+      borderWidth: 1,
+      borderColor: '#888',
+      borderRadius: 8,
+      paddingHorizontal: 15,
+      fontSize: 16,
+      backgroundColor: '#000',
+    },
+  });
+  
