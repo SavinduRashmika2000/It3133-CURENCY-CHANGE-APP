8045f17451bc8968860647c9cb79740a00d83a4a
Savindu_Rashmika <savindurashmika2000@gmail.com>
2024-12-25 15:55:23 +0530

    complete Header components

--- CurencyApp/components/Header.js
+++ CurencyApp/components/Header.js
@@ -4,7 +4,24 @@ import { PaperProvider, Text, Divider } from 'react-native-paper';
 export default function Header() {
     return (
         <PaperProvider>
-           <Text>aaaaaaaaaaaaaaaa</Text>
+        <View style={styles.container}>
+            <Text variant="displayMedium" style={styles.header}>Currency Converter</Text>
+            <Divider />
+        </View>    
         </PaperProvider>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ADD8E6', 
+        paddingVertical: 20,       
+        paddingHorizontal: 10,   
+        alignItems: 'center',     
+      },
+      headerText: {
+        color: '#000',             
+        textAlign: 'center',       
+        fontWeight: 'bold',       
+      },
+});
